1fcf086325b0fd6e7271f5e5692e3c1810691b9f
Noone <kasraam385@gmail.com>
2025-11-25 00:27:14 +0330

    contact be responsive

--- components/Contact/FormContact.tsx
+++ components/Contact/FormContact.tsx
@@ -6,12 +6,12 @@ import { Textarea } from "../ui/textarea";
 
 export default function FormContact() {
   return (
-    <div className="flex flex-col">
-      <h1 className="lg:text-7xl font-medium">
+    <div className="flex flex-col lg:w-3/5">
+      <h1 className="lg:text-7xl text-5xl text-center lg:text-start font-medium">
         We'd love to
         <br className="hidden lg:block" /> hear from you!
       </h1>
-      <p className="mt-5 opacity-80 lg:w-2/3">
+      <p className="mt-5 opacity-80 lg:w-2/3 text-center lg:text-start">
         Whether you have a project in mind, want to collaborate, or just have a
         question, feel free to reach out. Our team is ready to help.
       </p>
@@ -23,7 +23,7 @@ export default function FormContact() {
           type="text"
           name="userName"
           id="userName"
-          className="rounded-full py-6 font-medium lg:w-2/3"
+          className="rounded-full py-6 font-medium"
         />
         <Input
           style={{ fontSize: "1rem" }}
@@ -31,7 +31,7 @@ export default function FormContact() {
           type="email"
           name="userEmail"
           id="userEmail"
-          className="rounded-full py-6 font-medium lg:w-2/3"
+          className="rounded-full py-6 font-medium"
         />
         <Textarea
           style={{ fontSize: "1rem" }}
@@ -39,10 +39,10 @@ export default function FormContact() {
           name="userName"
           id="userName"
           maxLength={500}
-          className="rounded-xl py-6 font-medium lg:w-2/3 resize-none h-60"
+          className="rounded-xl py-6 font-medium resize-none h-60"
         />
         <Button
-          className="cursor-pointer rounded-full w-fit"
+          className="cursor-pointer rounded-full sm:w-fit"
           type="submit"
           variant={"default"}
         >
--- components/Contact/contact.tsx
+++ components/Contact/contact.tsx
@@ -1,10 +1,12 @@
-
+import Image from "next/image";
 import FormContact from "./FormContact";
 
 export default function Contact() {
   return (
-    <div className="grid lg:grid-cols-2 p-5 lg:p-10">
-      <div>{/* <Image src={`/`}/> */}</div>
+    <div className="flex flex-col-reverse lg:flex-row items-center gap-10 max-w-5xl mx-auto p-5 lg:p-10">
+      <div className="lg:w-2/5">
+        <Image src={`/nerdCat.jpg`} alt="nerd cat" width={900} height={400} />
+      </div>
       <FormContact />
     </div>
   );
