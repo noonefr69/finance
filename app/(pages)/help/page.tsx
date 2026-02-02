ae4d9bda91ca236e632e7045b27f099e511e8ea7
Noone <kasraam385@gmail.com>
2026-02-03 01:09:40 +0330

    nothing change just want to push

--- app/(pages)/help/page.tsx
+++ app/(pages)/help/page.tsx
@@ -14,7 +14,7 @@ export default function HelpPage() {
     <div className="flex">
       <div>Test</div>
 
-      <Card className="p-0 bg-transparent md:flex hidden text-muted-foreground text-[12px] border-0 shadow-nonesticky top-[calc(var(--header-height)+1px)] z-30 ml-auto h-[90svh] w-(--sidebar-width) flex-col gap-1 overflow-hidden overscroll-none pb-8">
+      <Card className="p-0 bg-transparent md:flex hidden text-muted-foreground text-[12px] border-0 shadow-none top-[calc(var(--header-height)+1px)] z-30 ml-auto h-[90svh] w-(--sidebar-width) flex-col gap-1 overflow-hidden overscroll-none pb-8">
         <CardHeader className="p-0">On This Page</CardHeader>
         <CardContent className="p-0">
           <p>Card Content</p>
@@ -22,7 +22,7 @@ export default function HelpPage() {
         </CardContent>
         <CardFooter className="mt-auto p-0">
           Give a star in github.
-          <Link href={`/`}>Kir</Link>
+          <Link target="_blank" href={`https://github.com/noonefr69/finance`}>Kir</Link>
         </CardFooter>
       </Card>
     </div>
